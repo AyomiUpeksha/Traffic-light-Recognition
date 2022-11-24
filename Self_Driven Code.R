22e06f65bc8b598783dd3392f0a035b077ae9c2c
AyomiUpeksha <69380485+AyomiUpeksha@users.noreply.github.com>
2022-11-24 07:38:26 +0530

    Update Self_Driven Code.R

--- Self_Driven Code.R
+++ Self_Driven Code.R
@@ -26,3 +26,27 @@ table(signs_pred, signs_actual)
 
 # Compute the accuracy
 mean(signs_pred == signs_actual)
+
+# Compute the accuracy of the baseline model (default k = 1)
+k_1 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types)
+mean(signs_actual == k_1)
+
+# Modify the above to set k = 7
+k_7 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k=7)
+mean(signs_actual == k_7)
+
+# Set k = 15 and compare to the above
+k_15 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k = 15)
+mean(signs_actual == k_15)
+
+# Use the prob parameter to get the proportion of votes for the winning class
+sign_pred <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types , k =7, prob = TRUE)
+
+# Get the "prob" attribute from the predicted classes
+sign_prob <- attr(sign_pred, "prob")
+
+# Examine the first several predictions
+head(sign_pred)
+
+# Examine the proportion of votes for the winning class
+head(sign_prob)
